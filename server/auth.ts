3d71b1e16288f39af1b3224776d3c61c7f3c3cd4
masahosono <dev.masahosono@gmail.com>
2021-03-28 13:13:32 +0900

    Add Express Custom Server

---
+++ server/auth.ts
@@ -0,0 +1,26 @@
+import { parse } from 'cookie';
+import jwt from 'jsonwebtoken';
+
+const auth = (req: any, res: any, next: any) => {
+    const cookie = req.headers.cookie;
+    if (cookie == undefined) {
+        return next(res.status(401));
+    }
+    const parseCookie = parse(req.headers.cookie);
+    const accessToken = parseCookie['x-access-token'];
+    const SECRET = process.env.SECRET || '';
+
+    if (accessToken) {
+        jwt.verify(accessToken, SECRET, (err: any) => {
+            if (err) {
+                return next(res.status(401));
+            }
+            next();
+        });
+    } else {
+        return next(res.status(401));
+    }
+};
+
+export default auth
+
