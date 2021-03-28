02745bc43bb09024516eeeaa54a4767a807c95ee
masahosono <dev.masahosono@gmail.com>
2021-03-28 12:53:42 +0900

    Add Config File

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    webpack: config => {
+        config.module.rules.push({
+            test: /\.(ts|tsx)$/,
+            use: {
+                loader: "ts-loader",
+            },
+        });
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg)$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 100000,
+                },
+            },
+        });
+        return config;
+    },
+};
