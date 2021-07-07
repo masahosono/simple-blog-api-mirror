d48c3bd0cdd93795026d936eb5461f2ab6d16096
masahosono <dev.masahosono@gmail.com>
2021-07-07 23:01:03 +0900

    server直下のファイルをリファクタ

--- server/api.ts
+++ server/api.ts
@@ -1,19 +1,20 @@
-import express from "express";
+import express from 'express'
 
-import multer from "multer";
-import fs from "fs";
-import moment from "moment";
-import auth from "./auth";
+import multer from 'multer'
+import fs from 'fs'
+import moment from 'moment'
+import auth from './auth'
 
-const apiRouter = express.Router();
+const apiRouter = express.Router()
 
+// Image Uploader
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const dir = 'public/image/article/' + moment().format('YYYYMMDD');
+        const dir = 'public/image/article/' + moment().format('YYYYMMDD')
         if (!fs.existsSync(dir)) {
             fs.mkdir(dir, () => {
                 cb(null, dir)
-            });
+            })
         } else {
             cb(null, dir)
         }
@@ -22,53 +23,56 @@ const storage = multer.diskStorage({
         cb(null, file.originalname)
     },
 })
-const upload = multer({ storage }).single('file');
+const upload = multer({ storage }).single('file')
 
 apiRouter.use((req, res, next) => {
-    console.debug(`[${req.url}]`, `[${req.method}]`, 'Request Received');
-    next();
-});
-
-apiRouter.get("/auth", auth, (req, res, next) => {
-    res.json({ success: true });
+    console.debug(`[${req.url}]`, `[${req.method}]`, 'Request Received')
+    next()
 })
 
-apiRouter.post("/image", auth, (req, res) => {
+apiRouter.get('/auth', auth, (req, res, next) => {
+    res.json({ success: true })
+})
+
+apiRouter.post('/image', auth, (req, res) => {
     upload(req, res, () => {
-        const url = process.env.FE_URL + '/' + req.file.path.replace('public/', '');
-        res.json({ imageUrl: url });
+        const url =
+            process.env.FE_URL + '/' + req.file.path.replace('public/', '')
+        res.json({ imageUrl: url })
     })
 })
 
-apiRouter.post("/article", auth, (req, res, next) => {
-    next();
+apiRouter.get('/article/category', auth, (req, res, next) => {
+    next()
 })
 
-apiRouter.get("/article/category", auth, (req, res, next) => {
-    next();
+apiRouter.post('/article/category', auth, (req, res, next) => {
+    next()
 })
 
-apiRouter.post("/article/category", auth, (req, res, next) => {
-    next();
+apiRouter.post('/article', auth, (req, res, next) => {
+    next()
 })
 
-apiRouter.put("/article/:id", auth, (req, res, next) => {
-    next();
+apiRouter.put('/article/:id', auth, (req, res, next) => {
+    next()
 })
 
-apiRouter.delete("/article/:id", auth, (req, res, next) => {
-    next();
+apiRouter.delete('/article/:id', auth, (req, res, next) => {
+    next()
 })
 
-apiRouter.get("/article", (req, res, next) => {
-    next();
-});
-apiRouter.post("/login", (req, res, next) => {
-    next();
-});
+// Not Auth
+apiRouter.get('/article', (req, res, next) => {
+    next()
+})
+apiRouter.post('/login', (req, res, next) => {
+    next()
+})
 
+// Error Handling
 apiRouter.use((err: any, req: any, res: any, next: any) => {
-    res.json({ success: false });
-});
+    res.json({ success: false })
+})
 
-export default apiRouter;
+export default apiRouter
--- server/be_server.ts
+++ server/be_server.ts
@@ -1,30 +1,33 @@
-import express from "express";
-import next from "next";
+import express from 'express'
+import next from 'next'
+
+// custom route
 import apiRoutes from './api'
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handler = app.getRequestHandler();
-const server = express();
- 
+// server
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handler = app.getRequestHandler()
+const server = express()
+
 app.prepare()
     .then(() => {
-        server.use(express.static("public"));
+        server.use(express.static('public'))
 
-        server.use("/api", apiRoutes, (req, res) => {
-            req.url = req.originalUrl;
-            return handler(req, res);
-        });
+        server.use('/api', apiRoutes, (req, res) => {
+            req.url = req.originalUrl
+            return handler(req, res)
+        })
 
-        server.all("*", (req, res) => {
-            return handler(req, res);
-        });
+        server.all('*', (req, res) => {
+            return handler(req, res)
+        })
 
         server.listen(8080, () => {
-            console.debug("> Ready on http://localhost:8080");
-        });
+            console.debug('> Ready on http://localhost:8080')
+        })
     })
     .catch(() => {
-        console.error();
-        process.exit(1);
-    });
+        console.error()
+        process.exit(1)
+    })
